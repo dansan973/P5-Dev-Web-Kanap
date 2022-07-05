c26236a5e6ce1cb4b332b390881197429231af76
dansan973 <97843023+dansan973@users.noreply.github.com>
2022-07-05 16:31:11 -0300

    recup et envoi Version createEl

--- front/js/script.js
+++ front/js/script.js
@@ -1,13 +1,49 @@
 //1) fetch dans api     
-        //recuperer les infos dans une vaiable "products"
-     //   2) parcourir les products et créer elements html et ensuite les envoyer dans la page html
+//recuperer les infos dans une vaiable "products"
+//   2) parcourir les products et créer elements html et ensuite les envoyer dans la page html
+let products = []
 fetch("http://localhost:3000/api/products", {
-  "method": "GET",
-  "headers": {}
+    "method": "GET",
+    "headers": {}
 })
+
+
 .then(response => {
-  console.log(response);
-})
-.catch(err => {
-  console.error(err);
-});
+        return response.json()
+    })
+    .then(products => {
+
+        listProductsInHtml(products)
+
+    })
+    .catch(err => console.error(err));
+
+function listProductsInHtml(products) {
+    let items = document.getElementById("items")
+
+    for (let index in products) {
+        let a = document.createElement("a")
+        a.setAttribute("href", "./product.html?id=" + products[index]._id)
+
+        let article = document.createElement("article")
+
+        let img = document.createElement("img")
+        img.setAttribute("src", products[index].imageUrl)
+        img.setAttribute("alt", products[index].altTxt)
+
+        let h3 = document.createElement("h3")
+        h3.setAttribute("class", "productName")
+        h3.innerHTML = "<strong>" + products[index].name + "</strong>"
+
+        let p = document.createElement("p")
+        p.setAttribute("class", "productDescription")
+        p.innerHTML = products[index].description
+
+        a.appendChild(article)
+        article.appendChild(img)
+        article.appendChild(h3)
+        article.appendChild(p)
+
+        items.appendChild(a)
+    }
+}
