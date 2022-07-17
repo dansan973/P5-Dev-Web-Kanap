147b0fa2f81d04af70ead043afa820a9aaca89da
dansan973 <97843023+dansan973@users.noreply.github.com>
2022-07-17 20:03:01 -0300

    infos utilisteur panier

--- front/js/cart.js
+++ front/js/cart.js
@@ -10,22 +10,42 @@ function setCart(cart) {
     localStorage.setItem("cart", JSON.stringify(cart));
 
 }
+
+function totalPrice() {
+    let price = 0;
+    let totalQuantity = 0
+    for (let c of canapes) {
+        price += c.price * c.quantity;
+        totalQuantity += c.quantity
+    }
+    console.log("totalprice")
+
+    const tPrice = document.getElementById('totalPrice')
+    const tQuantity = document.getElementById('totalQuantity')
+    tPrice.innerHTML = price
+    tQuantity.innerHTML = totalQuantity
+}
+
 //recuperation localstorage
 let cart = getCart()
-    //Alimentation du tableau 
+    //creation tab global
+let canapes = []
+
+
+//Alimentation du tableau à partir localstorage ezt API
 cart.forEach((product) => {
     const cartItems = document.getElementById("cart__items");
     const urlProduit = ` http://localhost:3000/api/products/${product.id}`;
 
-    // recuperation des elements et injection html avec createElement
+    // recuperation des elements et injection html avec createElement et appendChild
     fetch(urlProduit)
         .then((reponse) => {
             return reponse.json()
         })
         .then((data) => {
-            canape = { "id": product.id, "color": product.color, "quantity": product.quantity, "price": data.price, "name": data.name, "img": data.imageUrl, "alt": data.altTxt }
+            const canape = { "id": product.id, "color": product.color, "quantity": product.quantity, "price": data.price, "name": data.name, "img": data.imageUrl, "alt": data.altTxt }
             console.log(canape)
-
+            canapes.push(canape)
             let article = document.createElement("article")
             article.setAttribute("class", "cart__item")
             article.setAttribute("data-id", canape.id)
@@ -101,35 +121,120 @@ cart.forEach((product) => {
             divCartItemContentSettingsDelete.appendChild(pDel)
             divCartItemContentSettings.appendChild(divCartItemContentSettingsDelete)
 
-
-
-
             cartItems.appendChild(article)
 
 
 
-            /**!--  <article class="cart__item" data-id="{product-ID}" data-color="{product-color}">
-        <div class="cart__item__img">
-        <img src="../images/product01.jpg" alt="Photographie d'un canapé">
-        </div>
-        <div class="cart__item__content">
-          <div class="cart__item__content__description">
-            <h2>Nom du produit</h2>
-            <p>Vert</p>
-            <p>42,00 €</p>
-          </div>
-          <div class="cart__item__content__settings">
-            <div class="cart__item__content__settings__quantity">
-              <p>Qté : </p>
-              <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value="42">
-            </div>
-            <div class="cart__item__content__settings__delete">
-              <p class="deleteItem">Supprimer</p>
-            </div>
-          </div>
-        </div>
-      </article> -->
-      */
 
+
+
+
+
+            // modifification quantité avec addEventListener("change",fonction)
+            //remplacement dans le tableau( splice) puis reactualistion page
+
+            inputValue.addEventListener("change", (event) => {
+                const indexPosition = canapes.findIndex((item) => {
+                    return item.color === canape.color && item.id === canape.id
+                })
+                if (indexPosition === -1)
+                    return
+
+                canapes[indexPosition].quantity = parseInt(event.target.value)
+
+
+                console.log(event.target.value)
+
+                const newCart = canapes.map((canape) => {
+                    return { "id": canape.id, "color": canape.color, "quantity": canape.quantity }
+
+                })
+                setCart(newCart)
+                totalPrice()
+
+            })
+
+
+
+            // supprimer un item avec(elementClosest())
+            //remplacement dans le tableau( splice) puis reactualistion page
+
+
+
+            pDel.addEventListener("click", (event) => {
+                const indexPosition = canapes.findIndex((del) => {
+                        return del.color === canape.color && del.id === canape.id
+                    })
+                    //remove 
+                canapes.splice(indexPosition, 1)
+                const newCart = canapes.map((canape) => {
+                        return { "id": canape.id, "color": canape.color, "quantity": canape.quantity }
+
+                    })
+                    //local
+                setCart(newCart)
+                totalPrice()
+                    //dom
+                cartItems.removeChild(article)
+
+
+            })
+
+
+            //affichage html du  prix total 
+
+            totalPrice()
         })
 })
+
+
+
+
+// Récupérer les infos de l'utilisateur
+
+function testFirstName() {
+    let firstName = document.getElementById("firstName");
+    //minuscules, majuscules, les espaces et les tirets, chaine entre 3 et 20 caractères
+    let regexFirstName = /^[a-zA-Z\s-]{3,20}$/;
+
+    // addEnventListener 
+    firstName.addEventListener("change", function() {
+        // Si les conditions sont valides
+        if (regexFirstName.test(firstName.value) && firstName.value.length >= 3 && firstName.value.length < 20 && firstName.value !== "") {
+            letfirstNameErrorMsg = document.getElementById("firstNameErrorMsg");
+            firstNameErrorMsg.innerText = "";
+        }
+
+
+        // sinon message d'erreur
+        else {
+            let firstNameErrorMsg = document.getElementById("firstNameErrorMsg");
+            firstNameErrorMsg.innerText = "vérifier votre prénom";
+            return;
+        }
+    })
+
+
+
+
+
+    // lastname
+
+
+
+
+
+
+    // email check /minuscules, majuscules, les espaces et quelquelques caractères speciaux
+
+
+    /*      function ValidateEmail(inputText)
+            {
+    	    var mailformat = /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/
+    	    if(inputText.value.match(mailformat))
+    	    {
+    		alert("This is not a valid email address");
+    		return false;
+    		}
+    */
+}
