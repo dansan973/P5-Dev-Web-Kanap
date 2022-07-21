8697961fecee5c0b4c2d5b63c3ff87ebd64ca3d6
dansan973 <97843023+dansan973@users.noreply.github.com>
2022-07-21 11:11:21 -0300

    confirmation et vidange locst

--- front/js/confirmation.js
+++ front/js/confirmation.js
@@ -1,1 +1,9 @@
-const orderId = new URLSearchParams(window.location.search).get("orderId")
+// fonction recuperation ID via l'Url
+function confirmation() {
+
+    const orderId = new URLSearchParams(window.location.search).get("orderId")
+    document.getElementById("orderId").innerHTML += orderId;
+    // fonction vider le local storage
+    localStorage.clear()
+}
+confirmation()
